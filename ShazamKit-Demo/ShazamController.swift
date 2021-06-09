dcb5ac0a9ba7896a812448c1617ed0a24d8fe3a6
Aaryan Kothari <aaryan.kothari@gmail.com>
2021-06-09 17:38:05 +0530

    create: ShazamController

---
+++ ShazamKit-Demo/ShazamController.swift
@@ -0,0 +1,14 @@
+//
+//  ShazamController.swift
+//  ShazamKit-Demo
+//
+//  Created by Aaryan Kothari on 09/06/21.
+//
+
+import SwiftUI
+import ShazamKit
+
+class ShazamController: NSObject, ObservableObject, SHSessionDelegate {
+    
+
+}

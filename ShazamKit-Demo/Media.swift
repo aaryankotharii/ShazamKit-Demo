abc64770d36da81832ae24317dfc07ab70156004
Aaryan Kothari <aaryan.kothari@gmail.com>
2021-06-09 22:36:08 +0530

    Create: ShazamRow

---
+++ ShazamKit-Demo/Media.swift
@@ -0,0 +1,30 @@
+//
+//  Media.swift
+//  ShazamKit-Demo
+//
+//  Created by Aaryan Kothari on 09/06/21.
+//
+
+import Foundation
+import ShazamKit
+
+struct SHMedia {
+    
+    let name: String?
+    let artist: String?
+    let image: URL?
+    
+    init(media: SHMatchedMediaItem) {
+        self.name = media.title
+        self.artist = media.artist
+        self.image = media.artworkURL
+    }
+    
+    internal init(name: String?, artist: String?, image: URL?) {
+        self.name = name
+        self.artist = artist
+        self.image = image
+    }
+    
+    static let data = SHMedia(name: "Astronaut In The Ocean", artist: "Masked Wolf", image: URL(string: "https://is2-ssl.mzstatic.com/image/thumb/Music115/v4/83/eb/59/83eb59d9-629d-5f60-7a6b-a2cb0b70ebd1/075679793102.jpg/400x400cc.jpg"))
+}
---
+++ ShazamKit-Demo/ShazamRow.swift
@@ -0,0 +1,38 @@
+//
+//  ShazamRow.swift
+//  ShazamKit-Demo
+//
+//  Created by Aaryan Kothari on 09/06/21.
+//
+
+import SwiftUI
+
+struct ShazamRow: View {
+    let media: SHMedia
+    var body: some View {
+        HStack {
+            AsyncImage(url: media.image) { image in
+                image.resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 7))
+            } placeholder: {
+                Color.green
+            }
+            VStack(alignment:.leading,spacing:10) {
+                Text(media.name ?? "No Title")
+                    .font(.title2)
+                    .bold()
+                Text(media.artist ?? "")
+                    .font(.callout)
+            }
+            .padding()
+        }
+    }
+}
+
+struct ShazamRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ShazamRow(media: SHMedia.data)
+            .previewLayout(.sizeThatFits)
+    }
+}

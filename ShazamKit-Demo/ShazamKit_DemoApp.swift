2b33f8aaa8eb5b8a00048290e2a81bb7bb1cd1bb
Aaryan Kothari <aaryan.kothari@gmail.com>
2021-06-09 17:25:04 +0530

    Initial Commit

---
+++ ShazamKit-Demo/ShazamKit_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShazamKit_DemoApp.swift
+//  ShazamKit-Demo
+//
+//  Created by Aaryan Kothari on 09/06/21.
+//
+
+import SwiftUI
+
+@main
+struct ShazamKit_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
